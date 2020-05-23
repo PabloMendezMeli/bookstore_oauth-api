77dc0d88baf2a61aa4a664b7a7a3c7efb1493897
Pablo Mendez <pablo.mendez@mercadolibre.com>
2020-05-23 20:53:31 -0300

    adding cassandra connection

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/PabloMendezMeli/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/PabloMendezMeli/bookstore_oauth-api/src/domain/access_token"
 	"github.com/PabloMendezMeli/bookstore_oauth-api/src/utils/errors"
 )
@@ -16,5 +17,12 @@ type DbRepository interface {
 type dbRepository struct{}
 
 func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	//TODO: logic to get access_token from cassandra db
 	return nil, errors.NewInternalServerError("database connection not implemented yet")
 }
